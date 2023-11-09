b0267e31da420a16b21f0c1fc89b621c9bf1315c
soumilnimbalkar26 <soumilnimblkar@gmail.com>
2023-11-09 16:56:12 +0530

    feat: :sparkles: hoobank UI
    This is the complete UI of hoobank website. Completely responsive on every device

---
+++ hoobank/src/components/FeedbackCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { quotes } from "../assets";
+
+const FeedbackCard = ({ content, name, title, img }) => {
+  return (
+    <div className="flex flex-col justify-between px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img className="w-[42px] h-[27px] object-contain" src={quotes} alt="" />
+      <p className="font-poppins font-normal text-[18px] text-white my-10 leading-[32px]">
+        {content}
+      </p>
+      <div className="flex flex-row">
+        <img src={img} alt="name" className="w-[48px] h-[48px] rounded-full" />
+        <div className="flex flex-col ml-4">
+          <h4 className="font-poppins font-semibold text-[20px] text-white leading-[32px]">
+            {name}
+          </h4>
+          <p className="font-poppins font-normal text-[16px] text-dimWhite  leading-[24px]">
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedbackCard;
